6e386cd2425ce6a1c68c5f7bd2e1306a16994836
Aaron Ahn <aaron@countable.ca>
2022-07-04 22:58:50 -0700

    Responsive design

--- src/components/TextIntro.tsx
+++ src/components/TextIntro.tsx
@@ -4,7 +4,7 @@ export default function TextIntro(props: { onComplete: () => void }) {
   return (
     <div className="text-white font-body text-center opacity-90">
       <motion.div
-        className="relative inline-block w-[0px] overflow-x-hidden whitespace-nowrap font-mono"
+        className="relative text-3xl inline-block w-[0px] overflow-x-hidden whitespace-nowrap font-mono"
         initial={{ opacity: 0 }}
         animate={{
           width: "100%",
@@ -16,7 +16,7 @@ export default function TextIntro(props: { onComplete: () => void }) {
         onAnimationComplete={props.onComplete}
       >
         now i see
-        <motion.div className="text-2xs font-body">
+        <motion.div className="text-base font-body">
           @whispermeforever
         </motion.div>
       </motion.div>
